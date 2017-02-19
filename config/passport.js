ccdb6b2100fb141b8261ba77381a89147f8b9a40
Carolina Miranda <camrfn1@gmail.com>
2017-02-19 16:57:45 -0500

    Update strategy to use latest version

--- config/passport.js
+++ config/passport.js
@@ -1,32 +1,30 @@
+const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../app/models/user');
 const config = require('../config/db');
- 
-module.exports = function(passport) {
-  
-  var opts = {};
 
-  //secretOrKey required (contains the secret)
-  opts.secretOrKey = config.secret;
-  
-  //new JwtStrategy(opts, verify)
-  passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    
-    User.findOne({
-      id: jwt_payload.id
-    }, function(err, user) {
-          
-          if (err) {
-              return done(err, false);
-          }
-
-          if (user) {
-              done(null, user);
-          } else {
-              done(null, false);
-          }
-      });
-
-  }));
-
+const opts = {
+    secretOrKey: config.secret,
+    jwtFromRequest: ExtractJwt.fromAuthHeader()
 };
+
+passport.use(new JwtStrategy(opts, function(jwt_payload, cb) {
+    User.findOne({
+        id: jwt_payload.id
+    }, function(err, user) {
+
+        if (err) {
+            return cb(err, false);
+        }
+
+        if (user) {
+            cb(null, user);
+        } else {
+            cb(null, false);
+        }
+    });
+
+}));
+
+module.exports = passport;
